8ea4c6286ae14c00d17ae25731322f9cd60760b9
yum <yuma001209@gmail.com>
2022-04-25 19:28:15 +0900

    プレイヤー追加。移動少しバグってる

---
+++ ButiEngine_User/Player.cpp
@@ -0,0 +1,33 @@
+#include "stdafx_u.h"
+#include "Player.h"
+#include"InputManager.h"
+
+void ButiEngine::Player::OnUpdate()
+{
+	Move();
+}
+
+void ButiEngine::Player::OnSet()
+{
+}
+
+void ButiEngine::Player::Start()
+{
+	m_life = 3;
+	m_level = 1;
+	m_moveSpeed = 1.0f;
+}
+
+ButiEngine::Value_ptr<ButiEngine::GameComponent> ButiEngine::Player::Clone()
+{
+	return ObjectFactory::Create<Player>();
+}
+
+void ButiEngine::Player::Move()
+{
+	//XAZ•½–Ê‚ÌˆÚ“®•ûŒü‚ðŽæ“¾
+	Vector2 velocityXZ = InputManager::GetRightStick();
+	velocityXZ.Normalize();
+
+	gameObject.lock()->transform->Translate(Vector3(velocityXZ.x, 0.0f, velocityXZ.y));
+}
---
+++ ButiEngine_User/Player.h
@@ -0,0 +1,31 @@
+#pragma once
+#include"Header/GameComponentHeader.h"
+namespace ButiEngine {
+
+	class Player :public GameComponent
+	{
+	public:
+
+		std::string GetGameComponentName()const override {
+			return "Player";
+		}
+		void OnUpdate()override;
+		void OnSet()override;
+		void Start()override;
+		Value_ptr<GameComponent> Clone()override;
+		template<class Archive>
+		void serialize(Archive& archive)
+		{
+			archive(isActive);
+		}
+	private:
+		void Move();
+
+		int8_t m_life;
+		int8_t m_level;
+		float m_moveSpeed;
+	};
+
+}
+
+BUTI_REGIST_GAMECOMPONENT(Player, true);
