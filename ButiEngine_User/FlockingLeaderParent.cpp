c0733fac6c2f76ef8d8539400284bb3d0eca911b
yum <yuma001209@gmail.com>
2022-05-20 09:16:25 +0900

    モブハチ振り回し修正

--- ButiEngine_User/FlockingLeaderParent.cpp
+++ ButiEngine_User/FlockingLeaderParent.cpp
@@ -1,12 +1,21 @@
 #include "stdafx_u.h"
 #include "FlockingLeaderParent.h"
+#include "InputManager.h"
 
 void ButiEngine::FlockingLeaderParent::OnUpdate()
 {
 	Vector3 playerPos = m_vwp_player.lock()->transform->GetLocalPosition();
 	gameObject.lock()->transform->SetLocalPosition(playerPos);
 
-	m_vlp_lookAt->GetLookTarget()->SetLocalPosition(gameObject.lock()->transform->GetLocalPosition() + m_vwp_player.lock()->transform->GetFront() * 100.0f);
+	Vector2 rightStick = InputManager::GetRightStick();
+	rightStick.Normalize();
+
+	Vector3 dir = Vector3(rightStick.x, 0.0f, rightStick.y);
+
+	if (dir != Vector3Const::Zero)
+	{
+		m_vlp_lookAt->GetLookTarget()->SetLocalPosition(gameObject.lock()->transform->GetLocalPosition() + dir * 100.0f);
+	}
 }
 
 void ButiEngine::FlockingLeaderParent::OnSet()
@@ -37,5 +46,5 @@ void ButiEngine::FlockingLeaderParent::SetLookAtParameter()
 	m_vlp_lookAt = gameObject.lock()->GetGameComponent<LookAtComponent>();
 	m_vlp_lookAt->SetLookTarget(m_vwp_player.lock()->transform->Clone());
 	m_vlp_lookAt->GetLookTarget()->Translate(m_vwp_player.lock()->transform->GetFront());
-	m_vlp_lookAt->SetSpeed(1.0f);
+	m_vlp_lookAt->SetSpeed(0.1f);
 }
